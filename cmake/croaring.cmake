a277a5e30c98736093b76144ce6884521efed837
kosakseb <sebastian.kosak@tum.de>
2024-02-10 17:49:31 +0100

    added dyn dict

--- cmake/croaring.cmake
+++ cmake/croaring.cmake
@@ -7,17 +7,17 @@ find_package(Git REQUIRED)
 
 # Get croaring
 ExternalProject_Add(
-    croaring_src
-    PREFIX "vendor/croaring"
-    GIT_REPOSITORY "https://github.com/RoaringBitmap/CRoaring.git"
-    GIT_TAG b88b002407b42fafaea23ea5009a54a24d1c1ed4
-    TIMEOUT 10
-    CMAKE_ARGS
+        croaring_src
+        PREFIX "vendor/croaring"
+        GIT_REPOSITORY "https://github.com/RoaringBitmap/CRoaring.git"
+        GIT_TAG b88b002407b42fafaea23ea5009a54a24d1c1ed4
+        TIMEOUT 10
+        CMAKE_ARGS
         -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/vendor/croaring
         -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
         -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
         -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
-    UPDATE_COMMAND ""
+        UPDATE_COMMAND ""
 )
 
 # Prepare croaring
@@ -32,5 +32,5 @@ set_property(TARGET croaring APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${CRO
 # Dependencies
 add_dependencies(croaring croaring_src)
 
-install(FILES ${CROARING_LIBRARY_PATH} DESTINATION ${CMAKE_INSTALL_LIBDIR})
-install(DIRECTORY ${CROARING_INCLUDE_DIR} DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+install(FILES ${CROARING_LIBRARY_PATH} DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/croaring)
+install(DIRECTORY ${CROARING_INCLUDE_DIR} DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/cmake/croaring)
