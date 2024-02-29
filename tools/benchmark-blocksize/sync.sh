f5fb9abdf50ddb3f111bc1a718cca4fc28718ce3
kosakseb <sebastian.kosak@tum.de>
2024-02-29 21:56:57 +0100

    fix sync.sh

--- tools/benchmark-blocksize/sync.sh
+++ tools/benchmark-blocksize/sync.sh
@@ -31,7 +31,7 @@ sync_uris() {
     if [[ ! -d $bin_dir ]]; then
       ./csvtobtr --btr $btr_dir --binary $bin_dir --yaml ./csvtobtrdata/raw_data/$index/$schemaname --csv ./csvtobtrdata/raw_data/$index/$filename --create_binary true --create_btr true
     else
-      ./csvtobtr --btr $btr_dir --binary $bin_dir --yaml ./csvtobtrdata/raw_data/$index/$schemaname --csv ./csvtobtrdata/raw_data/$index/$filename --create_binary false --create_btr true
+      ./csvtobtr --btr $btr_dir --binary $bin_dir --yaml ./csvtobtrdata/raw_data/$index/$schemaname --csv ./csvtobtrdata/raw_data/$index/$filename --create_btr true
     fi
 
     echo "$factor, $filename, $(./decompression-speed --btr $btr_dir --reps 5)" >> $output_file
